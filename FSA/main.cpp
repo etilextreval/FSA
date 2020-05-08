f3edbcfb292271e61eb3a2a9cad80caa2348fe18
Alex Livrette <etilextreval@gmail.com>
2020-05-08 15:33:11 +0200

    Initial Commit

---
+++ FSA/main.cpp
@@ -0,0 +1,17 @@
+///************************************************************************/
+/*!
+@file main.cpp
+Original source by Alex Livrette (aka. etilextreval)
+released to the public domain
+License: MIT (see LICENSE)
+@brief
+*/
+/**************************************************************************/
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
